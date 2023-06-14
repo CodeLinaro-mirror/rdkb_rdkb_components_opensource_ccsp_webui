79c683b2fd2baf489543cc2e7d3bebbb1b2e826a
PKR Balireddy <pavan.balireddy@sky.uk>
2023-06-14 11:19:40 +0100

    RDKB-49314: Format issue with GUI "Operation In Progress" dialogue box
    Reason for change: Format issue with GUI "Operation In Progress" dialogue box
    
    Test Procedure:
    Test for GUI "Operation In Progress" dialogue box
    
    Risks: Low
    Priority: P2
    Signed-off-by: pavan.balireddy@sky.uk
    (cherry picked from commit 21df9e1c7d34b3f276b74554f5c317789e73f6af)
    Change-Id: I897bd3785b4af8d09a652a51dd57e551babb109b

--- source/Styles/xb3/cmn/js/lib/bootstrap-waitingfor.js
+++ source/Styles/xb3/cmn/js/lib/bootstrap-waitingfor.js
@@ -33,7 +33,7 @@
 					'<div class="modal-content">' +
 						'<div class="modal-header" style="display: none;"></div>' +
 						'<div class="modal-body">' +
-							'<div class="progress progress-striped active" style="margin-bottom:0;">' +
+							'<div class="progress progress-striped active">' +
 								'<div class="progress-bar" style="width: 100%"></div>' +
 							'</div>' +
 						'</div>' +
