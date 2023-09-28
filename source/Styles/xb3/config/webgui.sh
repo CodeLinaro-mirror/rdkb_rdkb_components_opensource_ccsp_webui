bb0052c13094f76775fe84e6606c5976f9ec0036
keerthana.p <keerthana_pandurangan@comcast.com>
2023-09-28 06:04:44 +0000

    RDKCOM-4333: REFPLTB-2741 : WebUI not working with latest turris image
    Reason for change: secure webui redirection support is not available in
    turris due to this,lighttpd process is not running at boot time
    Test Procedure: Check whether lighttpd is running
    Risks: Low
    Signed-off-by: keerthana.p <keerthana_pandurangan@comcast.com>
    Priority: P1
    
    (cherry picked from commit d421fab41dd6d4de83e8c06211f8153e9174b48e)
    Change-Id: I13dbba748a1c4b820385f6ae474b9a9354a469ed

--- source/Styles/xb3/config/webgui.sh
+++ source/Styles/xb3/config/webgui.sh
@@ -62,7 +62,7 @@ if [ -z $1 ] && [ ! -f /tmp/webuifwbundle ]; then
     fi
 fi
 
-if [ "x$BOX_TYPE" != "xrpi" ]; then
+if [ "x$BOX_TYPE" != "xrpi" ] && [ "x$BOX_TYPE" != "xturris" ]; then
 #upstreamed webgui_script_https_support.patch to Secure webui redirection as part of RDKB-42686.
 mkdir -p /tmp/.webui/
 ID="/tmp/trpfizyanrln"
