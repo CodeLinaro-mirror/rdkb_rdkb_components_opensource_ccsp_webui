7083e5e6a1fb4cfb43ddc3aa989e35984f40854d
rsingh013 <Roushan_Singh@comcast.com>
2024-07-02 06:30:47 +0000

    RDKB-56001: Replaced Gateway to Router in GUI page.
    Reason for change: Device type is Router.
    Test Procedure: Build and check in GUI page
    Risks: Low
    Priority: P1
    
    Signed-off-by: rsingh013 <Roushan_Singh@comcast.com>
    
    RDKB-55942: Removed MoCA from GUI page
    
    Reason for change: MoCA not supported
    Test Procedure: Build and check in GUI page
    Risks: Low
    Priority: P1
    
    Change-Id: Id1ca02aa634d3868704321fb9ffb3229390169f8
    Signed-off-by: rsingh013 <Roushan_Singh@comcast.com>
    
    RDKB-55976: Removed Docsis options from GUI page. Added Ethernet as WAN
    
    Reason for change: Docsis not supported
    Test Procedure: Build and check in GUI page
    Risks: Low
    Priority: P1
    
    Change-Id: Ifdfda44ddaeab9fde61e11e6e482a960f5b65c68
    Signed-off-by: rsingh013 <Roushan_Singh@comcast.com>
    
    RDKB-56051: Removed Brige Mode in GUI page.
    
    Reason for change: Bridge Mode not supported in router.
    Test Procedure: Build and check in GUI page
    Risks: Low
    Priority: P1
    
    Change-Id: I0fdda2b6f6474a7a233207a00a0f376da49a9aae
    Signed-off-by: rsingh013 <Roushan_Singh@comcast.com>
    
    RDKB-55654: WPS available in GUI with the encryption WPA3-Personal Only
    
    Reason for change: WPS option is available in GUI
    with the encryption WPA3-Personal Only
    
    Test Procedure:
    Test for WPS availability in GUI with the encryption WPA3-Personal Only
    
    Change-Id: I4c45362f604f5971d444654ad35c16d0d19b0bb6
    Risks: Low
    Priority: P2
    Signed-off-by: pavan.balireddy@sky.uk
    (cherry picked from commit 97f74fc677ba0c1865050a0a40ca01ada0be6813)
    
    RDKB-56084: Removed association of ethernet port2 to Xfinity Home from GUI page
    
    Reason for change: Not supported option from GUI page.
    Test Procedure: Build and check in GUI page
    Risks: Low
    Priority: P1
    
    Change-Id: I0fbd60a6661cf8c80b97dab4983feff7dd824d27
    Signed-off-by: rsingh013 <Roushan_Singh@comcast.com>
    (cherry picked from commit e0dd962beedaa34eb34b8a2294c741a945124d9b)
    (cherry picked from commit 443ded770affc6bad24ff573d53890e14d127856)
    
    RDKB-56287: Adding WPS GUI changes
    
    Reason for change: Adding WPS GUI changes
    Test Procedure: Build and verify in GUI page.
    Risks: Low
    Priority: P1
    
    Change-Id: I1a21f165009aa77fb4830e761d687b961451592b
    Signed-off-by: rsingh013 <Roushan_Singh@comcast.com>
    
    RDKB-56281: Adding Battery GUI changes
    
    Reason for change: Adding Battery GUI changes
    Test Procedure: Build and verify in GUI page.
    Risks: Low
    Priority: P1
    
    Change-Id: Ie47c244e0cbe651e7b6fa4d74075596cc8f77733
    Signed-off-by: rsingh013 <Roushan_Singh@comcast.com>
    (cherry picked from commit cfed8118bdcdf2ccfef5a8729b863a86b4955f05)
    
    XER10-438: Two adjacent grey bands on Xfinity network page
    
    Reason for change: Two adjacent grey bands on Xfinity network page
    Test Procedure: Build and check in XER10 GUI page
    Risks: Low
    Priority: P1
    
    Change-Id: I29b3f68a4d7cbc707b176dcde22495f5b764618f
    Signed-off-by: rsingh013 <Roushan_Singh@comcast.com>
    
    XER10-456: Visual pattern is not showing as expected on Wi-Fi Configuration page
    
    Reason for change: Visual pattern is not showing as expected on Wi-Fi Configuration page
    Test Procedure: Build and verify in GUI page.
    Risks: Low
    Priority: P1
    
    Change-Id: I2872662e3032523ad16712d859311d72c77e8308
    Signed-off-by: rsingh013 <Roushan_Singh@comcast.com>
    
    XER5-1101: "Edit" option should be removed from WAN Network
    
    Reason for change: "Edit" option should be removed from WAN Network
    Test Procedure: Build and verify in GUI page.
    Risks: Low
    Priority: P1
    
    Change-Id: Ic2c48ba06121db9783b315f40d94e7662d3a995a
    Signed-off-by: rsingh013 <Roushan_Singh@comcast.com>
    (cherry picked from commit 2292385741bb070a5ae5eaa6e14ba46ef8ec4424)
    
    XB10-668: Removing WAN Network page from XB10
    
    Reason for change: Removing WAN Network page from XB10
    Test Procedure: Build and verify in XB10 GUI page.
    Risks: Low
    Priority: P1
    
    Change-Id: I903857a1dd975c5838c05f9d15303a7fdf6b640b

--- source/Styles/xb3/jst/locale/global.js
+++ source/Styles/xb3/jst/locale/global.js
@@ -242,6 +242,8 @@ $("[id^='chipadd']").text($.i18n("Change the IP address assignment method for On
 $("p[id^='connedittip']").text($.i18n("If DHCP is selected, the Gateway's DHCP server will automatically assign the IP address."));
 $("p[id^='connedittip2']").html($.i18n("If Reserved IP is selected, the IP address will be fixed without DHCP operation and you'll need to manually enter the IP address. The IP address must be within the DHCP IP address pool. To find your IP address range, go to <strong>Gateway > Connection > Local IP Network.</strong>"));
 $("p[id^='connedittip3']").text($.i18n("Reserved IP addresses can be assigned to any device that acts as a server or that requires a fixed IP address."));
+$("p[id^='connedittip4']").text($.i18n("If DHCP is selected, the Router's DHCP server will automatically assign the IP address."));
+$("p[id^='connedittip5']").html($.i18n("If Reserved IP is selected, the IP address will be fixed without DHCP operation and you'll need to manually enter the IP address. The IP address must be within the DHCP IP address pool. To find your IP address range, go to <strong>Router > Connection > Local IP Network.</strong>"));
 $("#editdevloc,#editdevloc1").text($.i18n("Edit Device"));
 $("label[id^='confloc']").text($.i18n("Configuration:"));
 $("span[id^='idyes']").text($.i18n("Yes"));
